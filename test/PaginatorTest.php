fc1174ff662efb1a2c2f6e5db1f737832003087f
fedys <miroslav.fedeles@gmail.com>
2016-07-19 11:22:24 +0200

    #22 avoid repetitive call $this->_calculatePageCount() in count() method if count equals zero

--- test/PaginatorTest.php
+++ test/PaginatorTest.php
@@ -147,6 +147,23 @@ class PaginatorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(101, $paginator->getTotalItemCount());
     }
 
+    public function testRepetitiveCallCountResultingZeroDoesNotCauseRepetitiveCallOfCalculatePageCountMethod()
+    {
+        $count = 0;
+
+        $paginator = $this->getMockBuilder(Paginator\Paginator::class)
+            ->setConstructorArgs([new Adapter\ArrayAdapter([])])
+            ->setMethods(['_calculatePageCount'])
+            ->getMock();
+
+        $paginator->expects($this->once())
+            ->method('_calculatePageCount')
+            ->willReturn($count);
+
+        $this->assertEquals($count, $paginator->count());
+        $this->assertEquals($count, $paginator->count());
+    }
+
     public function testLoadsFromConfig()
     {
         Paginator\Paginator::setGlobalConfig($this->config->testing);
